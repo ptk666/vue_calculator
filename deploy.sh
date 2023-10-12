93a22d385d660ad9af341c159bf7eff7e4c04844
ptk666 <potingkay@gmail.com>
2023-10-12 14:06:33 +0800

    updated script

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m "updated"
-git push -f git@github.com:ptk666/vue_calculator.git master
+git push -f git@github.com:ptk666/vue_calculator.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/vue_calculator/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue_calculator/' : '/',
   transpileDependencies: true
 })
